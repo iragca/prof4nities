fa92be52e6b517f89c976e1ab1b2be0ae8986f3f
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-10-26 18:45:37 +0800

    feat: add Language enum for supported languages

---
+++ prof4nities/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Language(Enum):
+    ENGLISH = "en"
+    SPANISH = "es"
+    FRENCH = "fr"
+    GERMAN = "de"
+    ITALIAN = "it"
+    PORTUGUESE = "pt"
+    RUSSIAN = "ru"
+    CHINESE = "zh"
+    JAPANESE = "ja"
+    KOREAN = "ko"
